9d9e165c786043a534d86f448b4a3c3d8fc7a433
pipao1229 <126714967+pipao1229@users.noreply.github.com>
2024-03-22 17:36:10 -0600

    main actualizado
    errores corregidos, excepciones agregadas

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 //
-//  main.cpp
+//  Función main
 //  Tarea2ED
 //
 //  Creado por Felipe Benavides y Nathaniel Motykiewicz
@@ -7,17 +7,13 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <string> // Incluir la cabecera para string
 #include "List.h"
 #include "ArrayList.h"
 #include "LinkedList.h"
 #include "DLinkedList.h"
 
-using std::string;
-using std::cin;
-using std::cout;
-using std::endl;
-using std::find;
-using std::boolalpha;
+using namespace std; // Usar esto para evitar escribir std::
 
 // Función para generar un número aleatorio entre min y max
 int random(int min, int max) {
@@ -26,183 +22,231 @@ int random(int min, int max) {
 
 int main() {
     setlocale(LC_CTYPE, "Spanish");
-    
+
     string tipoList;
-        List<int>* L = nullptr;
+    List<int>* L = nullptr;
 
-        while (true) { //sirve
-            cout << "Que desea utilizar, ArrayList, LinkedList o DLinkedList? (A/L/D) \nCual sería su opción?: ";
-            getline(cin, tipoList);
+    while (true) {
+        cout << "¿Qué desea utilizar, ArrayList, LinkedList o DLinkedList? (A/L/D)" << endl;
+        cout << "¿Cuál sería su opción?: ";
+        getline(cin, tipoList);
 
-            if (tipoList == "A") {
-                L = new ArrayList<int>();
-                break;
-            } else if (tipoList == "L") {
-                L = new LinkedList<int>();
-                break;
-            } else if (tipoList == "D") {
-                L = new DLinkedList<int>();
-                break;
-            } else {
-                cout << "Por favor ingrese una opción válida" << endl;
+        // Según la selección del usuario:
+        if (tipoList == "A") { // Crear ArrayList
+            L = new ArrayList<int>();
+            break;
+        }
+        else if (tipoList == "L") { // Crear LinkedList
+            L = new LinkedList<int>();
+            break;
+        }
+        else if (tipoList == "D") { // Crear DLinkedList
+            L = new DLinkedList<int>();
+            break;
+        }
+        else {
+            cout << "Por favor ingrese una opción válida" << endl;
+        }
+    }
+
+    while (true) {
+        string menu = "Las operaciones que se pueden realizar son:\n insert, append, remove, clear, getElement, goToStart, goToEnd, goToPos, next, previous, atStart, atEnd, getPos, getSize, indexOf, contains, cargarLista y salir\n ¿Qué opción desea realizar?: ";
+
+        cout << menu;
+        string respuestaMenu;
+        getline(cin, respuestaMenu);
+
+        if (respuestaMenu == "insert") {
+            cout << "¿Cuál número desea insertar?: ";
+            string numAInsert;
+            cin >> numAInsert;
+
+            try {
+                int numeroInsert = stoi(numAInsert); // Intentar convertir el string a un número entero
+
+                L->insert(numeroInsert);
+            }
+            catch (const invalid_argument& e) { // Capturar la excepción si la conversión falla
+                cout << "Error: Debe ingresar un número entero válido." << endl;
             }
         }
-    
-    while (true) { //sirve
-    string menu = "las operaciones que se pueden realizar son:\n insert, append, remove, clear, getElement,goToStart, goToEnd, goToPos, next, previous, atStart, atEnd, getPos, getSize, indexOf, contains, cargarLista y salir \nCual opcion desea realizar?: ";
-    
-    cout << menu;
-    string respuestaMenu;
-    getline(cin, respuestaMenu);
-    
-    
-    if (respuestaMenu == "insert"){ //este sirve bien
-        cout << "cual numero desea insertar?: " << endl;
-        int numeroInsert;
-        cin >> numeroInsert;
-        if (numeroInsert % 1 != 0){
-            menu = "error"; }
-        L->insert(numeroInsert);
-    }
-    
-    else if (respuestaMenu == "append"){ //este sirve bien
-        cout << "cual numero desea insertar?: " << endl;
-        int numeroAppend;
-        cin >> numeroAppend;
-        if (numeroAppend % 1 != 0){
-            menu = "error"; }
-        L->append(numeroAppend);
-    }
-    
-    else if (respuestaMenu == "remove"){ //sirve pero hay que darle enter dos veces no se pq
-        L->remove();
-    }
-    
-    else if (respuestaMenu == "clear"){ // sirve bien igual hay que hacer el doble enter
-        L->clear();
-        cout << "se ha limpiado la lista" << endl;
-    }
-    
-    else if (respuestaMenu == "getElement"){ // sirve bien
-        int elementoPos = L->getElement();
-        cout << "el elemento de la posicion actual es: " << elementoPos << endl;
-    }
-    
-    else if (respuestaMenu == "goToStart"){ //sirve bien
-        L->goToStart();
-        cout << "se ha vuelto al inicio de la lista" << endl;
-    }
-    
-    else if (respuestaMenu == "goToEnd"){ //sirve bien
-        L->goToEnd();
-        cout << "se ha ido al final de la lista" << endl;
-    }
-    
-    else if (respuestaMenu == "goToPos"){ //sirve bien
-        cout << "a que posicion desea ir: " << endl;
-        int numeroPosicion;
-        cin >> numeroPosicion;
-        if (numeroPosicion % 1 != 0){
-            menu = "error"; }
-        L->goToPos(numeroPosicion);
-        cout << "se ha ido a la posicion" << numeroPosicion << endl;
-    }
-    
-    else if (respuestaMenu == "next"){ //sirve bien
-        L->next();
-        cout << "se ha movido a la siguiente posicion" << endl;
-    }
-    
-    else if (respuestaMenu == "previous"){ //sirve bien
-        L->previous();
-        cout << "se ha movido a la posicion anterior";
-    }
-    
-    else if (respuestaMenu == "atStart"){ // sirve con bool
-        bool boolAtStart = L->atStart();
-        if (boolAtStart == 1){
-            boolAtStart = true; }
-        else{
-            boolAtStart = false;
+        else if (respuestaMenu == "append") {
+            cout << "¿Cuál número desea insertar?: ";
+            string numAppend;
+            cin >> numAppend;
+
+            try {
+                int numeroAppend = stoi(numAppend); // Intentar convertir el string a un número entero
+
+                L->append(numeroAppend);
+            }
+            catch (const invalid_argument& e) { // Capturar la excepción si la conversión falla
+                cout << "Error: Debe ingresar un número entero válido." << endl;
+            }
         }
-        cout << "la posicion está al inicio: " << boolAtStart << endl;
-    }
-    
-    else if (respuestaMenu == "atEnd"){ // sirve
-        bool boolAtEnd = L->atEnd();
-        if (boolAtEnd == 1){
-            boolAtEnd = true; }
-        else{
-            boolAtEnd = false;
+        else if (respuestaMenu == "remove") {
+            try {
+                L->remove();
+                cout << "Se eliminó el elemento actual\nUse enter para continuar" << endl;
+            }
+            catch (const runtime_error& e) {
+                cerr << "Error: " << e.what() << endl;
+            }
         }
-        cout << "la posicion está al final : " << boolAtEnd << endl;
-    }
-    
-    else if (respuestaMenu == "getPos"){ // sirve
-        int pos = L->getPos();
-        cout << "esta en la posición: " << pos << endl;
-    }
-    
-    else if (respuestaMenu == "getSize"){ // sirve
-        int sizeLista = L->getSize();
-        cout << "el tamaño de la lista es de:" << sizeLista << endl;
-        
-    }
-    
-    else if (respuestaMenu == "indexOf"){ // sirve
-        cout << "para que numero le desea averiguar su indice?: " << endl;
-        int numeroIndice;
-        cin >> numeroIndice;
-        int indiceNum = L->indexOf(numeroIndice);
-        if (numeroIndice % 1 != 0){
-            menu = "error"; }
-        cout << "el indice de el numero " << numeroIndice << " es " << indiceNum << endl;
-    }
-    
-    else if (respuestaMenu == "contains"){ // no retorna el bool, sino 1 o 0, no se como cambiarlo
-        cout << "que numero desea saber si está en la lista? : " << endl;
-        int numeroContains;
-        cin >> numeroContains;
-        bool contieneNum = L->contains(numeroContains);
-        if (numeroContains % 1 != 0){
-            menu = "error";}
-        if (contieneNum == 1){
-            contieneNum = true; }
+        else if (respuestaMenu == "clear") { 
+            L->clear();
+            cout << "Se ha limpiado la lista\nUse enter para continuar" << endl;
+        }
+        else if (respuestaMenu == "getElement") {
+            try {
+                int elementoPos = L->getElement();
+                cout << "El elemento de la posición actual es: " << elementoPos << endl;
+                cout << "Use enter para continuar" << endl;
+            }
+            catch (const runtime_error& e) {
+                cerr << "Error: " << e.what() << endl;
+            }
+        }
+        else if (respuestaMenu == "goToStart") { 
+            L->goToStart();
+            cout << "Se ha vuelto al inicio de la lista\nUse enter para continuar" << endl;
+        }
+        else if (respuestaMenu == "goToEnd") { 
+            L->goToEnd();
+            cout << "Se ha ido al final de la lista\nUse enter para continuar" << endl;
+        }
+        else if (respuestaMenu == "goToPos") {
+            cout << "A qué posición desea ir: ";
+            string numPos;
+            cin >> numPos;
+
+            try {
+                int numeroPosicion = stoi(numPos); // Intentar convertir el string a un número entero
+
+                L->goToPos(numeroPosicion);
+                cout << "Se ha ido a la posición " << numeroPosicion << endl;
+            }
+            catch (const invalid_argument& e)  { // Capturar la excepción si la conversión falla
+                cout << "Error: Debe ingresar un número entero válido.\nUse enter para continuar" << endl;
+            }
+            catch (const runtime_error& e) {
+                cerr << "Error: " << e.what() << endl;
+            }
+        }
+        else if (respuestaMenu == "next") {
+            if (!L->atEnd()) { // Verificar si no se ha llegado al final de la lista
+                L->next();
+                cout << "Se ha movido a la siguiente posición\nUse enter para continuar" << endl;
+            }
+            else {
+                cout << "Ya se ha llegado al final de la lista\nUse enter para continuar" << endl;
+            }
+        }
+        else if (respuestaMenu == "previous") {
+            if (!L->atStart()) { // Verificar si no se ha llegado al inicio de la lista
+                L->previous();
+                cout << "Se ha movido a la posición anterior\nUse enter para continuar" << endl;
+            }
+            else {
+                cout << "Ya se ha llegado al inicio de la lista\nUse enter para continuar" << endl;
+            }
+        }
+        else if (respuestaMenu == "atStart") { 
+            bool boolAtStart = L->atStart();
+            if (boolAtStart == true) {
+                cout << "Te encuentras en la primera posicion\nUse enter para continuar" << endl;
+            }
+            else {
+                cout << "NO te encuentras en la primera posición\nUse enter para continuar" << endl;
+            }
+        }
+        else if (respuestaMenu == "atEnd") { 
+            bool boolAtEnd = L->atEnd();
+            if (boolAtEnd == true) {
+                cout << "Te encuentras en la última posición\nUse enter para continuar" << endl;
+            }
+            else {
+                cout << "NO te encuentras en la última posición\nUse enter para continuar" << endl;
+            }
+        }
+        else if (respuestaMenu == "getPos") { 
+            int pos = L->getPos();
+            cout << "Estás en la posición: " << pos << endl;
+            cout << "Use enter para continuar" << endl;
+        }
+        else if (respuestaMenu == "getSize") { 
+            int sizeLista = L->getSize();
+            cout << "El tamaño de la lista es de: " << sizeLista << endl;
+            cout << "Use enter para continuar" << endl;
+        }
+        else if (respuestaMenu == "indexOf") {
+            cout << "Para qué número desea averiguar su índice?: ";
+            string indice;
+            cin >> indice;
+
+            try {
+                int numeroIndice = stoi(indice); // Intentar convertir el string a un número entero
+
+                int indiceNum = L->indexOf(numeroIndice);
+                cout << "El índice del número " << numeroIndice << " es " << indiceNum << endl;
+            }
+            catch (const invalid_argument& e) { // Capturar la excepción si la conversión falla
+                cout << "Error: Debe ingresar un número entero válido." << endl;
+            }
+        }
+        else if (respuestaMenu == "contains") {
+            cout << "Qué número desea saber si está en la lista?: " << endl;
+            string numContains;
+            cin >> numContains;
+
+            try {
+                int numeroContains = stoi(numContains); // Intentar convertir el string a un número entero
+
+                bool contieneNum = L->contains(numeroContains);
+
+                if (contieneNum) {
+                    cout << "El número " << numeroContains << " está en la lista" << endl;
+                }
+                else {
+                    cout << "El número " << numeroContains << " NO está en la lista" << endl;
+                }
+            }
+            catch (const invalid_argument& e) { // Capturar la excepción si la conversión falla
+                cout << "Error: Debe ingresar un número entero válido." << endl;
+            }
+        }
+        else if (respuestaMenu == "cargarLista") {
+            cout << "Con cuántos números aleatorios desea cargar la lista?: ";
+            string numLista;
+            cin >> numLista;
+
+            try {
+                int cantidadNumeros = stoi(numLista); // Intentar convertir el string a un número entero
+
+                for (int i = 0; i < cantidadNumeros; i++) {
+                    int numeroRandom = random(1, 1000); // Función para generar números aleatorios
+                    L->append(numeroRandom);
+                }
+                cout << "Se ha cargado la lista con éxito\n" << endl;
+            }
+            catch (const invalid_argument& e) { // Capturar la excepción si la conversión falla
+                cout << "Error: Debe ingresar un número entero válido." << endl;
+            }
+        }
+        else if (respuestaMenu == "salir") { 
+            cout << "Gracias por usar nuestro programa" << endl;
+            break;
+            }
         else {
-            contieneNum = false;
-        }
-        cout << "el numero " << numeroContains << " está en la lista: " << contieneNum << endl;
-            
+                cout << "\nPor favor ingrese una opción válida\nUse enter para continuar";
+                }
+
+                cin.ignore();
+                L->print();
+                L->getPos();
+                cout << "\n";
     }
-    else if (respuestaMenu == "cargarLista"){ //sirve bien
-        cout << "con cuantos numero aleatorios desea cargar la lista? :";
-        int cantidadNumeros;
-        cin >> cantidadNumeros;
-        for (int i = 0; i < cantidadNumeros; i++ ) {
-            int numeroRandom = random(1,1000); //funcion en la linea 22
-            L->append(numeroRandom);
-        }
-        cout << "se ha cargado la lista con exito\n" << endl;
-        
-    }
-            
-    else if (respuestaMenu == "salir"){
-        cout << "gracias por usar nuestro programa" << endl;
-        break; // solo con salir se ejecuta el break
-        }
-            
-        
-    else {
-        cout << "\npor favor ingrese una opcion valida\nuse enter para continuar";
-    }
-            
-        cin.ignore(); // si se ingresa una opcion no valida se borra y se vuelve a ejecutar el while
-        L->print();
-        L->getPos();
-        cout << "\n";
-        
-    } //while del menu principal
-    
-    delete L; //sacar estos dos del while que sino se borra la lista
+
+    delete L;
     return 0;
 }
